4a49a18cfb027d7c83d27f7b5e13f410693380f3
Rustam Mahmudov <rusyaxam@yandex.ru>
2024-02-04 13:19:28 +0300

    Sprint_6 Фиксы по замечаниям. Удалил файлик LionTestParameterizesOld, хотя его делал для себя и специально весь закомментировал. В fileneTest разделил негативные и положительные проверки. У меня на работе наоборот стараемся меньше дробить.

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -17,27 +17,36 @@ public class FelineTest {
     @Mock
     Animal animal;
     @Test
-    public void eatMeat() throws Exception {
+    public void eatMeatTrue() throws Exception {
         Mockito.when(animal.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));  // Создал стаб, который возвращает нужное значение для метода
         List<String> expected = List.of("Животные", "Птицы", "Рыба"); // создание и заполнение списка
-        List<String> notExpected = List.of("Трава", "Различные растения"); // создание и заполнение списка
         assertEquals("Проверка кем питается хищник", expected, animal.getFood("Хищник"));
+    }
+    @Test
+    public void eatMeatFalse() throws Exception {
+        List<String> notExpected = List.of("Трава", "Различные растения"); // создание и заполнение списка
         assertNotEquals("Негативная Проверка кем питается хищник", notExpected, feline.getFood("Хищник"));
     }
 
     @Test
-    public void getFamily() {
-//        Mockito.when(feline.getFamily()).thenReturn("Кошачьи"); // Создал стаб, который возвращает нужное значение для метода
+    public void getFamilyTrue() {
         assertEquals("Проверка на Кошачьи", "Кошачьи", feline.getFamily());
+    }
+    @Test
+    public void getFamilyFalse() {
         assertNotEquals("Негативная Проверка на Кошачьи", "Медвежьи", feline.getFamily());
     }
 
     @Test
-    public void getKittens() {
+    public void getKittensTrue() {
         Feline feline = new Feline();
         assertEquals("Проверка на количество", 1, feline.getKittens());
-        assertNotEquals("Негативная Проверка на количество", 2, feline.getKittens());
         assertEquals("Проверка на количество с передачей параметра", 5, feline.getKittens(5));
+    }
+    @Test
+    public void getKittensFalse() {
+        Feline feline = new Feline();
+        assertNotEquals("Негативная Проверка на количество", 2, feline.getKittens());
         assertNotEquals("Негативная Проверка на количество с передачей параметра", 5, feline.getKittens(8));
     }
 
